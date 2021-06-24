7f8e69a210f39991ef75bcb1c9118510e2905241
Kacper Faber <kacperf1234@gmail.com>
2021-06-24 11:01:00 +0200

    AuthenticationService works now

--- src/main/java/com/talker/websocketlibrary/authentication/AuthenticationService.java
+++ src/main/java/com/talker/websocketlibrary/authentication/AuthenticationService.java
@@ -1,10 +1,12 @@
 package com.talker.websocketlibrary.authentication;
 
 import com.talker.websocketlibrary.database.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+import java.util.List;
 
 @Component
 public class AuthenticationService {
@@ -24,16 +26,20 @@ public class AuthenticationService {
             put("usernameOrEmail", usernameOrEmail);
         }};
 
-        User user = sessionFactory
-                .getCurrentSession()
-                .createQuery("from User where (username = :usernameOrEmail or email = :usernameOrEmail) and passwordHash = :passwordHash", User.class)
-                .setProperties(properties)
-                .getSingleResult();
+        Session session = sessionFactory
+                .getCurrentSession();
 
-        if (user == null) {
+        session.beginTransaction();
+
+        List<User> users = session
+                .createQuery("from User where (upper(username) = upper(:usernameOrEmail) or upper(email) = upper(:usernameOrEmail)) and passwordHash = :passwordHash", User.class)
+                .setProperties(properties)
+                .getResultList();
+
+        if (users.isEmpty()) {
             return new AuthenticationResult(false, null);
         }
 
-        return new AuthenticationResult(true, user.getId());
+        return new AuthenticationResult(true, users.stream().findFirst().get().getId());
     }
 }
