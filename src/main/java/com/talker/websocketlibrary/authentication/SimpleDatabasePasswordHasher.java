c639b65c73190158d0d7c78eea0dc487d916edef
Kacper Faber <kacperf1234@gmail.com>
2021-07-04 13:08:11 +0200

    Fixed one issue, one received request 'userId' (should point user sender) it was requested user. Line was copied from SentRelationshipRequest generator, and that.

--- src/main/java/com/talker/websocketlibrary/authentication/SimpleDatabasePasswordHasher.java
+++ src/main/java/com/talker/websocketlibrary/authentication/SimpleDatabasePasswordHasher.java
@@ -1,8 +1,11 @@
 package com.talker.websocketlibrary.authentication;
 
+import org.springframework.stereotype.Component;
+
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
+@Component
 public class SimpleDatabasePasswordHasher implements IPasswordHasher {
     @Override
     public String hash(String password) {
