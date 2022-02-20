383e7717680f42239584790741c6a4e30919bc68
Kacper Faber <kacperf1234@gmail.com>
2022-02-20 17:08:08 +0100

    added IResultInterpreter and NullResultInterpreter (optional empty)

---
+++ webSocketLibrary/src/main/java/com/talker/websocketlibrary/reflections/IResultInterpreter.java
@@ -0,0 +1,8 @@
+package com.talker.websocketlibrary.reflections;
+
+import java.util.Optional;
+
+public interface IResultInterpreter {
+    boolean canInterpret(Optional<Object> methodInvokerResult);
+    void interpret(Optional<Object> methodInvokerResult);
+}
---
+++ webSocketLibrary/src/main/java/com/talker/websocketlibrary/reflections/NullResultInterpreter.java
@@ -0,0 +1,18 @@
+package com.talker.websocketlibrary.reflections;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class NullResultInterpreter implements IResultInterpreter{
+    @Override
+    public boolean canInterpret(Optional<Object> methodInvokerResult) {
+        return methodInvokerResult.isEmpty();
+    }
+
+    @Override
+    public void interpret(Optional<Object> methodInvokerResult) {
+
+    }
+}
