82593cf9cc4e1daa1291df33fcc55662d4b35a6d
Kacper Faber <kacperf1234@gmail.com>
2022-02-21 11:33:48 +0100

    added tests 'MethodInvokerResultInterpreter_interpret_Optional_Tests'
    Changed 'MethodInvokerResultInterpreter' - deleted autowired injection, added constructor.

--- webSocketLibrary/src/main/java/com/talker/websocketlibrary/reflections/MethodInvokerResultInterpreter.java
+++ webSocketLibrary/src/main/java/com/talker/websocketlibrary/reflections/MethodInvokerResultInterpreter.java
@@ -1,6 +1,5 @@
 package com.talker.websocketlibrary.reflections;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -8,8 +7,12 @@ import java.util.Optional;
 
 @Component
 public class MethodInvokerResultInterpreter implements IMethodInvokerResultInterpreter{
-    @Autowired
-    public List<IResultInterpreter> interpreterList;
+    public final List<IResultInterpreter> interpreterList;
+
+    public MethodInvokerResultInterpreter(List<IResultInterpreter> interpreterList) {
+        this.interpreterList = interpreterList;
+    }
+
     @Override
     public void interpret(Optional<Object> methodInvokerResult) {
         interpreterList.stream().filter(x -> x.canInterpret(methodInvokerResult)).forEach(x -> x.interpret(methodInvokerResult));
---
+++ webSocketLibrary/src/test/java/com/talker/websocketlibrary/reflections/tests/MethodInvokerResultInterpreter_interpret_Optional_Tests.java
@@ -0,0 +1,37 @@
+package com.talker.websocketlibrary.reflections.tests;
+
+import com.talker.websocketlibrary.reflections.IResultInterpreter;
+import com.talker.websocketlibrary.reflections.MethodInvokerResultInterpreter;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class MethodInvokerResultInterpreter_interpret_Optional_Tests {
+    void exec(Optional<Object> opt, List<IResultInterpreter> list) {
+        new MethodInvokerResultInterpreter(list).interpret(opt);
+    }
+    @Test
+    public void doesNotThrow() {
+        IResultInterpreter mock = mock(IResultInterpreter.class);
+        when(mock.canInterpret(any())).thenReturn(true);
+        ArrayList<IResultInterpreter> list = new ArrayList<>() {{add(mock);}};
+        Assertions.assertDoesNotThrow(() -> exec(Optional.empty(), list));
+    }
+    @Test
+    public void asksCanInterpretMethodOfEachListElement() {
+        IResultInterpreter mock1 = mock(IResultInterpreter.class);
+        when(mock1.canInterpret(any())).thenReturn(true);
+        IResultInterpreter mock2 = mock(IResultInterpreter.class);
+        when(mock2.canInterpret(any())).thenReturn(true);
+        ArrayList<IResultInterpreter> list = new ArrayList<>() {{add(mock1); add(mock2);}};
+        exec(Optional.empty(), list);
+        verify(mock1, times(1)).canInterpret(any());
+        verify(mock2, times(1)).canInterpret(any());
+    }
+
+    // TODO: finish it.
+}
