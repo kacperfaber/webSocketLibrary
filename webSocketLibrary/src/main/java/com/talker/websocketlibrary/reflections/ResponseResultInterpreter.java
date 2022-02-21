80d0f298999671b131464ae73a0cdea102f5c713
Kacper Faber <kacperf1234@gmail.com>
2022-02-21 12:13:50 +0100

    Created ResponseResultInterpreter (interprets ResponseResult, active when optional is not empty and optional object class is ResponseResult)

---
+++ webSocketLibrary/src/main/java/com/talker/websocketlibrary/reflections/ResponseResultInterpreter.java
@@ -0,0 +1,27 @@
+package com.talker.websocketlibrary.reflections;
+
+import com.talker.websocketlibrary.messaging.MessageService;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+public class ResponseResultInterpreter implements IResultInterpreter{
+    public MessageService messageService;
+
+    public ResponseResultInterpreter(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @Override
+    public boolean canInterpret(Optional<Object> methodInvokerResult) {
+        return methodInvokerResult.filter(o -> o.getClass() == ResponseResult.class).isPresent();
+    }
+
+    @Override
+    public void interpret(Optional<Object> methodInvokerResult, Command command) throws IOException {
+        ResponseResult response = (ResponseResult) methodInvokerResult.get();
+        messageService.sendToSession(command.getSessionId(), response.name, response.data, response.dataClass);
+    }
+}
